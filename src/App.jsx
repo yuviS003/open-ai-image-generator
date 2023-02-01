29153ae854c89525718a28482b67a4326f57ba9e
Yuvraj Singh <yuvi7860808034@gmail.com>
2023-02-01 16:12:55 +0530

    loader added

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import "./App.css";
 function App() {
   const [prompt, setPrompt] = useState("");
   const [imageUrl, setImageUrl] = useState("");
+  const [loader, setLoader] = useState(false);
   const configuration = new Configuration({
     apiKey: import.meta.env.VITE_OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
   const generateImage = async () => {
+    setLoader(true);
     const response = await openai.createImage({
       prompt,
       n: 1,
@@ -18,11 +20,13 @@ function App() {
     let image_url = response["data"];
     console.log(image_url.data[0].url);
     setImageUrl(image_url.data[0].url);
+    setLoader(false);
   };
 
   return (
     <div className="App">
       <h3>Generate Images using OPEN AI</h3>
+      {loader ? <h5>Generating... Kindly wait</h5> : null}
       <input
         type="text"
         value={prompt}
@@ -30,7 +34,7 @@ function App() {
         placeholder="Enter text for which you want the image"
       />
       <button onClick={generateImage}>Generate Image</button>
-      <img src={imageUrl} alt="image" />
+      {imageUrl.length > 0 ? <img src={imageUrl} alt="image" /> : null}
     </div>
   );
 }
